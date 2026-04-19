0e9033e6278251d27e2bd7036782d71bd3fce01d
Aakashgupta <gameweb94@gmail.com>
2026-04-19 20:02:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin:"releafy-frontend.vercel.app",
+  origin:"https://releafy-frontend.vercel.app",
   credentials:true
 })
 app.use(express.json());
