a11341bfa66992eb8e1fb1392270c104d46f04ed
Aakashgupta <gameweb94@gmail.com>
2026-04-19 19:56:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin:"https://releafy.onrender.com",
+  credentials:true
+})
 app.use(express.json());
 
 // MongoDB connection
